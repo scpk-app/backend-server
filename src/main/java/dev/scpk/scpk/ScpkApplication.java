25f766b55c59e5a81acda98b530f21426db82e59
Piotr Tylczynski <piotr@ptl.cloud>
2021-02-22 18:06:06 +0100

    init

---
+++ src/main/java/dev/scpk/scpk/ScpkApplication.java
@@ -0,0 +1,13 @@
+package dev.scpk.scpk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScpkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/scpk/scpk/ScpkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.scpk.scpk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
