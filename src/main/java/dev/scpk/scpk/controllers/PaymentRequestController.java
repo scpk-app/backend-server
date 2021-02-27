6de311b1ed74b4181482bfc9719bfcd5c819230c
Piotr Tylczynski <piotr@ptl.cloud>
2021-02-27 23:34:44 +0100

    logging in controllers add

--- src/main/java/dev/scpk/scpk/controllers/PaymentRequestController.java
+++ src/main/java/dev/scpk/scpk/controllers/PaymentRequestController.java
@@ -12,6 +12,9 @@ import dev.scpk.scpk.hateoas.assembler.PaymentRequestASM;
 import dev.scpk.scpk.hateoas.model.full.PaymentGroupModel;
 import dev.scpk.scpk.hateoas.model.full.PaymentRequestModel;
 import dev.scpk.scpk.services.PaymentRequestService;
+import dev.scpk.scpk.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -33,12 +36,23 @@ public class PaymentRequestController {
     @Autowired
     private PaymentRequestASM paymentRequestASM;
 
+    @Autowired
+    private UserService userService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @PatchMapping("/add")
     public PaymentGroupModel addPayment(
             @RequestParam("payment_group_id")PaymentGroupDAO paymentGroupDAO,
             @RequestParam("charged_users_id")UserDAO[] chargedUsers,
             @RequestParam("value") Double value
     ) throws UserDoesNotExistsException, ObjectNotHashableException, InsufficientPermissionException, PaymentGroupDoesNotExistsException {
+        this.logger.info(
+                "Serving add of payment {} with charged users {} with value {}",
+                paymentGroupDAO.toString(),
+                Arrays.toString(chargedUsers),
+                value.toString()
+        );
         PaymentRequestDAO paymentRequestDAO = this.paymentRequestService.create(
                 Arrays.asList(chargedUsers),
                 paymentGroupDAO,
@@ -54,6 +68,11 @@ public class PaymentRequestController {
             @RequestParam("payment_group_id") PaymentGroupDAO paymentGroupDAO,
             @RequestParam("payment_request_id") PaymentRequestDAO paymentRequestDAO
     ) throws UserDoesNotExistsException, InsufficientPermissionException, ObjectNotHashableException {
+        this.logger.info(
+                "Serving removal of payment request {} from group {}",
+                paymentGroupDAO.toString(),
+                paymentRequestDAO.toString()
+        );
         PaymentGroupDAO paymentGroupDAO1 =
                 this.paymentRequestService.removePaymentRequest(paymentGroupDAO, paymentRequestDAO);
         return this.paymentGroupASM.toModel(paymentGroupDAO1)
@@ -64,6 +83,11 @@ public class PaymentRequestController {
     public CollectionModel<PaymentRequestModel> getInWhichCharged(
             @RequestParam("payment_group_id") PaymentGroupDAO paymentGroupDAO
     ) throws UserDoesNotExistsException {
+        this.logger.info(
+                "Serving retrieval of charges for user {} in group {}",
+                this.userService.getLoggedInUser().toString(),
+                paymentGroupDAO.toString()
+        );
         List<PaymentRequestDAO> paymentRequestDAOList =
                 this.paymentRequestService.getAllInWhichUserCharged(paymentGroupDAO);
         return this.paymentRequestASM.toCollectionModel(paymentRequestDAOList)
@@ -74,6 +98,11 @@ public class PaymentRequestController {
     public CollectionModel<PaymentRequestModel> getInWhichRequests(
             @RequestParam("payment_group_id") PaymentGroupDAO paymentGroupDAO
     ) throws UserDoesNotExistsException {
+        this.logger.info(
+                "Serving ordered payments for user {} in group {}",
+                this.userService.getLoggedInUser().toString(),
+                paymentGroupDAO.toString()
+        );
         List<PaymentRequestDAO> paymentRequestDAOList =
                 this.paymentRequestService.getAllRequested(paymentGroupDAO);
         return this.paymentRequestASM.toCollectionModel(paymentRequestDAOList)
