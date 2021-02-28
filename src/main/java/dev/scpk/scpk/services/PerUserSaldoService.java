3100e283fed7620df8f1f54180ab26f92adfd54e
Piotr Tylczynski <piotr@ptl.cloud>
2021-02-28 12:34:19 +0100

    per user slado service logged

--- src/main/java/dev/scpk/scpk/services/PerUserSaldoService.java
+++ src/main/java/dev/scpk/scpk/services/PerUserSaldoService.java
@@ -8,6 +8,8 @@ import dev.scpk.scpk.exceptions.UserDoesNotExistsException;
 import dev.scpk.scpk.exceptions.security.ObjectNotHashableException;
 import dev.scpk.scpk.repositories.PerUserSaldoRepository;
 import dev.scpk.scpk.security.acl.AccessLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,14 @@ public class PerUserSaldoService {
     @Autowired
     private ACLService aclService;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public PerUserSaldoDAO createPerUserSaldo(UserDAO recipient, UserBalanceDAO userBalanceDAO) throws ObjectNotHashableException, UserDoesNotExistsException {
+        this.logger.debug(
+                "Creating Saldo for user {} in user balance list {}",
+                recipient.toString(),
+                userBalanceDAO.toString()
+        );
         PerUserSaldoDAO perUserSaldoDAO = new PerUserSaldoDAO();
         perUserSaldoDAO.setValue(0d);
         perUserSaldoDAO.setUserBalance(userBalanceDAO);
@@ -35,6 +44,12 @@ public class PerUserSaldoService {
     }
 
     public List<PerUserSaldoDAO> createPerUserSaldoAndUpdateOthers(UserBalanceDAO userBalanceDAO, PaymentGroupDAO paymentGroupDAO) throws ObjectNotHashableException, UserDoesNotExistsException {
+        this.logger.debug(
+                "Creating new Saldo for user {} and in balance {} and updating all other user balances in group {}",
+                userBalanceDAO.getUser().toString(),
+                userBalanceDAO.toString(),
+                paymentGroupDAO.toString()
+        );
         List<PerUserSaldoDAO> result = new ArrayList<>();
         List<UserDAO> participants = paymentGroupDAO.getParticipants();
         UserDAO newUser = userBalanceDAO.getUser();
@@ -44,6 +59,9 @@ public class PerUserSaldoService {
                     this.createPerUserSaldo(participant, userBalanceDAO)
             );
         }
+        this.logger.trace(
+                "Created new user balance"
+        );
         // update other per user saldos
         int balancesNumber = paymentGroupDAO.getUserBalances().size();
         for(int i = 0; i < balancesNumber; i++){
@@ -56,10 +74,17 @@ public class PerUserSaldoService {
                );
            }
         }
+        this.logger.trace(
+                "Updated other user balances"
+        );
         return result;
     }
 
     public void remove(PerUserSaldoDAO perUserSaldoDAO){
+        this.logger.debug(
+                "Removing user slado {}",
+                perUserSaldoDAO.toString()
+        );
         this.perUserSaldoRepository.delete(perUserSaldoDAO);
     }
 }
